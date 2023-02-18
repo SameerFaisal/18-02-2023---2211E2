866113565077c96106360100a3ac3932fc1377ba
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-18 16:50:38 +0500

    18-02-2023

---
+++ 18-02-2023 (2211E2)/app.js
@@ -0,0 +1,18 @@
+// let str="I love Pakistan"
+// alert(str.length)
+// alert(str.indexOf("a"))
+// alert(str.lastIndexOf("a"))
+// alert(str.charAt(16))
+// var arr=[5,2,1,8,6,3]
+// document.write(arr+"<br>")
+// document.write(arr.reverse())
+// document.write(arr.sort())
+// arr.sort()
+
+// document.write(arr.reverse())
+
+
+// let str="I LOVE MY COUNTRY"
+// let str="i love my country"
+// document.write(str.toUpperCase())
+
